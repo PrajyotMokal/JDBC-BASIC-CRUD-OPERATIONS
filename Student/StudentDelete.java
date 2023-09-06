2150257831e934d9476b1da446911a1783a1bd59
PrajyotMokal <prajyot.mokal@gmail.com>
2023-09-06 09:48:50 +0530

    JDBC BASIC CRUD OPERATIONS

---
+++ Student/StudentDelete.java
@@ -0,0 +1,44 @@
+package Student;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class StudentDelete {
+	public static void main(String[] args) {
+		String url = "jdbc:mysql://localhost:3306/javabatch";
+		String username = "root";
+		String password = "root";
+		String sql = "DELETE FROM student where id=2";
+		Connection connection = null;
+
+		// STEP 1: Load/Register the driver
+
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			// Establish Connection
+			connection = DriverManager.getConnection(url, username, password);
+
+			// Establish Statement
+			Statement statement = connection.createStatement();
+			statement.execute(sql);
+
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				connection.close();
+				System.out.println("All good");
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+	}
+}
---
+++ Student/StudentGet.java
@@ -0,0 +1,52 @@
+package Student;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class StudentGet {
+	public static void main(String[] args) {
+		String url = "jdbc:mysql://localhost:3306/javabatch";
+		String username = "root";
+		String password = "root";
+		String sql = "SELECT * FROM student where id=2";
+		Connection connection = null;
+
+		// STEP 1: Load/Register the driver
+
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			// Establish Connection
+			connection = DriverManager.getConnection(url, username, password);
+
+			// Establish Statement
+			Statement statement = connection.createStatement();
+			ResultSet resultSet = statement.executeQuery(sql);
+
+			while (resultSet.next()) {
+				System.out.println(resultSet.getInt(1));
+				System.out.println(resultSet.getString(2));
+				System.out.println(resultSet.getString(3));
+				System.out.println("===========================");
+			}
+
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				connection.close();
+				System.out.println("All good");
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+	}
+}
